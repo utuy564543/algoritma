c42c5a58895319b34b727b4bd71ab3bb9f9ccd26
utuy564543 <144861518+utuy564543@users.noreply.github.com>
2023-11-08 08:01:13 +0700

    Add files via upload

---
+++ Algoritma/script.js
@@ -0,0 +1,6 @@
+let Nilai = parseInt(prompt("masukan nilai :", ""));
+if (Nilai < 70){
+    document.write("MOHON MAAF ANDA TIDAK LULUS" + Nilai);
+}else{
+    document.write("SELAMAT ANDA LULUS" + Nilai )
+}
